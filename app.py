b9b5004f1160fcf3573b6a52d527a82eafd99268
sshah98 <shahsuraj261@gmail.com>
2018-09-15 13:42:24 -0400

    added map functionality and linked it

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+    
+@app.route('/map')
+def map():
+    return render_template('hurrycane_map.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
-    
